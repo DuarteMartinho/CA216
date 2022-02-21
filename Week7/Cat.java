e18413e8c3ab2f3630955a7e4a263991adb10e6c
DuarteMartinho <duartemartinho11@gmail.com>
2022-02-21 23:05:49 +0000

    week 7 Lab Pt 1

---
+++ Week7/Cat.java
@@ -0,0 +1,13 @@
+package Week7;
+
+// Task 1 + 2 + 3
+public class Cat extends Animal {
+
+    public Cat(String n) {
+        super(n);
+    }
+
+    String hello() {
+        return "Meow";
+    }
+}
---
+++ Week7/Pig.java
@@ -0,0 +1,13 @@
+package Week7;
+
+// Task 4
+public class Pig extends Animal {
+
+    public Pig(String n) {
+        super(n);
+    }
+
+    String hello() {
+        return "Oink";
+    }
+}
